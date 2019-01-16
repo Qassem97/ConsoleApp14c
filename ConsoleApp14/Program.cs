8fdf8f8d3bcdcdac91a632446b9003ace507bc01
qassem.aburas@outlook.com <qassem.aburas@outlook.com>
2019-01-16 13:50:13 +0100

    Add project files.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime timeNow = DateTime.Now;    // Gets current system time
+
+            Console.WriteLine(timeNow);
+            Console.WriteLine(timeNow.ToString());    // same as above
+            Console.WriteLine(timeNow.ToShortDateString());
+            Console.WriteLine(timeNow.ToLongDateString());
+            Console.WriteLine(timeNow.ToString("yy-dd-MM"));
+
+            DateTime timeMod = timeNow.AddHours(-1);
+
+            Console.WriteLine(timeMod);
+
+            TimeSpan timeSpan = new TimeSpan(12, 45, 30);
+
+            DateTime timeModSpan = timeNow.Add(timeSpan);
+
+            Console.WriteLine(timeModSpan);
+
+            Console.ReadKey();
+
+            DoWhileWorkTime(timeModSpan);
+
+            DateTime myB_Day = new DateTime(1997, 06, 05);
+
+            Console.WriteLine(myB_Day);
+
+            Console.ReadKey();
+        }
+
+        static void DoWhileWorkTime(DateTime time)
+        {
+            DateTime endOfWorkDay = time;
+
+            do
+            {
+                Console.WriteLine(endOfWorkDay.ToShortTimeString());
+                endOfWorkDay = endOfWorkDay.AddHours(1);
+            } while
+            (endOfWorkDay.Hour < 16 || (endOfWorkDay.Hour <= 16 && endOfWorkDay.Minute <= 30));
+
+        }
+    }
+}
